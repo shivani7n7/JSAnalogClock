0276eb6294befff8b71d70b30b40f7854d6e5078
shivani7n7 <shivanimehta7n7@gmail.com>
2020-05-09 08:00:55 +0530

    color change

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ const HOURHAND = document.querySelector('#hour');
 const MINUTEHAND = document.querySelector('#minute');
 const SECONDHAND = document.querySelector('#second');
 
+var date = new Date();
+
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+let hrPosition = hr * 30 + min * 6 / 12;
+let minPosition = min * 6 + sec * 6 / 60;
+let secPosition = sec * 6;
+
 function runTheClock() {
-	var date = new Date();
-
-	let hr = date.getHours();
-	let min = date.getMinutes();
-	let sec = date.getSeconds();
-
-	let hrPosition = hr * 30 + (min * 6) / 12;
-	let minPosition = min * 6 + (sec * 6) / 60;
-	let secPosition = sec * 6;
+	hrPosition += 1 / 120;
+	minPosition += 1 / 10;
+	secPosition += 6;
 
 	HOURHAND.style.transform = 'rotate(' + hrPosition + 'deg)';
 	MINUTEHAND.style.transform = 'rotate(' + minPosition + 'deg)';
